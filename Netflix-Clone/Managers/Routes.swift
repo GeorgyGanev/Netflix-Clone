a6b5f3306e70d9c43e88874b2a6e99b01176e228
Georgy Ganev <106162345+GeorgyGanev@users.noreply.github.com>
2024-07-03 19:16:07 +0300

    update routes content

--- Netflix-Clone/Managers/Routes.swift
+++ Netflix-Clone/Managers/Routes.swift
@@ -9,8 +9,8 @@ import Foundation
 
 struct Constants {
     
-    static let API_KEY = "?api_key=dd4e9a76dbd313f1a0d2e3a37c59f6e6"
-    static let UTUBE_API_KEY = "AIzaSyA8BcBHE8Atsr8qE80j_cD--4LCY7gLiCM"
+    static let API_KEY = "?api_key=a3718f4656bb449b88e6db6bb38c1876"
+    static let UTUBE_API_KEY = "AIzaSyCOH7oIL5OW_g6LLLPRb7ZCbOWWRFRc0fE"
     static let mainUrl = "https://api.themoviedb.org/3"
     static let imageBaseUrl = "https://image.tmdb.org/t/p/w500"
     static let uTubeBaseUrl = "https://www.googleapis.com/youtube/v3/search"
